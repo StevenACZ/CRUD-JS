76f94d2b54b72cfe7d225791e0393110ed3380fb
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-03-09 18:34:19 -0500

    Article pet

--- src/js/app.js
+++ src/js/app.js
@@ -53,26 +53,80 @@ btnAddPet.addEventListener('click', function() {
   container.classList.add('active');
 })*/
 
-function init() {
-  const form = document.querySelector('.modal__body');
+function getValues() {
   const inputName = document.querySelector("[name='name']");
   const inputLastname = document.querySelector("[name='lastname']");
   const inputBreed = document.querySelector("[name='breed']");
   const inputPhone = document.querySelector("[name='phone']");
   const inputCountry = document.querySelector("[name='country']");
   const inputPhoto = document.querySelector("[name='photo']");
+  const inputAbout = document.querySelector("[name='about']");
+
+  return {
+    inputName: inputName.value,
+    inputLastname: inputLastname.value,
+    inputBreed: inputBreed.value,
+    inputPhone: inputPhone.value,
+    inputCountry: inputCountry.value,
+    inputPhoto: inputPhoto.value,
+    inputAbout: inputAbout.value
+  }
+}
+
+function init() {
+  const form = document.querySelector('.modal__body');
 
   form.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log('inputName', inputName.value);
-    console.log('inputLastName', inputLastname.value);
-    console.log('inputName', inputBreed.value);
-    console.log('inputName', inputPhone.value);
-    console.log('inputName', inputPhoto.value);
+    insertCard(createCard(getValues()));
+    let btnAddPet = document.querySelector('.js_btnAddPet');
+    let btnAddPetHeader = document.querySelector('.js_btnAddPetHeader');
+    btnAddPet.style.display = 'none';
+    btnAddPetHeader.style.display = 'block';
   })
 }
 
+function insertCard(card) {
+  const main = document.querySelector('.main');
+  main.appendChild(card);
+}
 
+function createCard(values) {
+  const {
+    inputName: name,
+    inputLastname: lastname,
+    inputBreed: breed,
+    inputPhone: phone,
+    inputCountry: country,
+    inputPhoto: photo,
+    inputAbout: about
+  } = values;
+  const card = document.createElement('article');
+  card.classList.add('pet');
+  card.innerHTML = `
+    <button class="pet__edit js_edit">
+      <img src="./src/icons/edit.png">
+      Edit
+    </button>
+    <button class="pet__delete js_delete">
+      <img src="./src/icons/delete.png">
+      Delete
+    </button>
+
+    <img class="pet__img" src="${photo}" alt="A pet">
+
+    <div class="pet__information">
+      <p class="pet__name">${name}</p>
+      <p><span class="pet__phone">${phone}</span> | <span class="pet__email">${breed}</span></p>
+      <p class="pet__country">${country}</p>
+    </div>
+
+    <p class="pet__about">
+      ${about}
+    </p>
+  `
+  return card;
+}
 
 
 init();
